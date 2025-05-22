855e823af6f965551815a30510ba980603c12c4c
Everton da Rosa <everton3x@gmail.com>
2025-05-22 11:37:06 -0300

    Second test

--- src/AutoloadFilesPlugin.php
+++ src/AutoloadFilesPlugin.php
@@ -70,4 +70,8 @@ class AutoloadFilesPlugin implements PluginInterface, EventSubscriberInterface
         $packageRoot = realpath(dirname($this->composer->getConfig()->getConfigSource()->getName()));
         return str_replace($packageRoot . DIRECTORY_SEPARATOR, '', $absolutePath);
     }
+
+    public function deactivate(Composer $composer, IOInterface $io) {}
+
+    public function uninstall(Composer $composer, IOInterface $io) {}
 }
